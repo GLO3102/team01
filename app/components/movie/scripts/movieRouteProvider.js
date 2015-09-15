6db419b1799faece9dc7ce30dd6cca7c8b9f2173
Antoine Lefrancois <antoine.lefrancois.1@ulaval.ca>
2015-09-14 22:02:48 -0400

    Added static movie from back-end, added controller for movie detail

--- app/components/movie/scripts/movieRouteProvider.js
+++ app/components/movie/scripts/movieRouteProvider.js
@@ -1,6 +1,11 @@
 movieApp.
     config(['$routeProvider', function ($routeProvider) {
 
-        $routeProvider.when('/movies', {"templateUrl": "app/components/movie/views/movie.html"}).otherwise({redirectTo: '/'});
-
+        $routeProvider
+            .when('/movies', {"templateUrl": "app/components/movie/views/movie.html"})
+            .when("/movies/{movieId}",
+                {
+                    templateUrl: "app/components/views/movieDetail.html",
+                    controller: "movie-detail-controller"
+                });
     }]);
