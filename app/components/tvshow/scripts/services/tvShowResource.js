684a1a20ad19240a4cc8f37e8a95dfe7fd014d41
jsroy19 <jean-sebastien.roy2@mail.mcgill.ca>
2015-10-05 16:24:39 -0400

    Ajouter l'API pour le reload (pas dans le dashboard) des TVShow.. reste à faire un peu comme pour movies dans la page tvshow (choix parmi des genres??)

---
+++ app/components/tvshow/scripts/services/tvShowResource.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Jean-Sebastien on 2015-10-05.
+ */
+tvShowApp.factory('tvShowResource', ["$resource", function($resource){
+    return $resource("https://umovie.herokuapp.com/unsecure/tvshows/season/:id",{} ,{
+        get:{
+            method:"GET",
+            params:{
+                id:"@id"
+            }
+        }
+
+    });
+}]);
---
+++ app/components/tvshow/scripts/services/tvShowSearchResource.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Jean-Sebastien on 2015-10-05.
+ */
+tvShowApp.factory('tvShowSearchResource', ["$resource", function ($resource) {
+    return $resource("https://umovie.herokuapp.com/unsecure/search/:type/seasons", {}, {
+        query: {
+            method: "GET",
+            isArray: false,
+            params: {
+                "type": "tvshows"
+            }
+        }
+    });
+}]);
