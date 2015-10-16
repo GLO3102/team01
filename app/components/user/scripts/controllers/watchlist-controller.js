8f8acc790cd5e0023044b790485e72d57d0ae426
Antoine Lefrancois <antoine.lefrancois.1@ulaval.ca>
2015-10-16 00:29:39 -0400

    Added watchlist page

--- app/components/user/scripts/controllers/watchlist-controller.js
+++ app/components/user/scripts/controllers/watchlist-controller.js
@@ -4,14 +4,71 @@
 /**
  * Created by Antoine on 2015-10-15.
  */
-userApp.controller("watchlist-controller", function ($scope, loggedUserService, watchlistResource) {
-    $scope.loggedUser = loggedUserService.getLoggedUser();
+userApp.controller("watchlist-controller", function ($scope, loggedUserService, watchlistResource, userWatchlistContainer) {
 
-    $scope.fetchLoggedUserWatchlist = function (){
+    $scope.loggedUser = loggedUserService.getLoggedUser();
+    $scope.userWatchlist = [];
+
+    $scope.initLoggedUserWatchlist = function () {
+
+        var userWatchlist = userWatchlistContainer.getUserWatchlist();
+
+        if (Object.keys(userWatchlist).length == 0) {
+            fetchLoggedUserWatchlist();
+        }
+        else {
+            $scope.userWatchlist = userWatchlist.watchlists;
+        }
+    };
+
+    var fetchLoggedUserWatchlist = function () {
         watchlistResource.query({}, function onSuccess(data) {
-            console.log(data);
+            var userWatchlist = [];
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].hasOwnProperty("owner")) {
+                    if (data[i].owner.email === $scope.loggedUser) {
+                        userWatchlist.push(data[i]);
+                    }
+                }
+            }
+            userWatchlistContainer.setUserWatchlist(userWatchlist);
+            $scope.userWatchlist = userWatchlist;
+        })
+    };
+
+    $scope.removeMovieFromWatchlist = function (watchlist, movie) {
+        watchlistResource.deleteMovieFromWatchList({
+                "id": watchlist.id,
+                "trackId": movie.trackId
+            }, function onSuccess(data) {
+                updateLocalWatchlist(watchlist, movie);
+            }
+        )
+    }
+    $scope.removeWatchlist = function(watchlist){
+        watchlistResource.deleteWatchlist({
+            "id":watchlist.id
+        }, function onSuccess(){
+            $scope.userWatchlist.filter(function (watchlistInLocal) {
+                return watchlistInLocal.id == watchlist.id;
+            }).pop();
+            userWatchlistContainer.setUserWatchlist($scope.userWatchlist);
+            $scope.$apply();
         })
     }
+    var updateLocalWatchlist = function (watchlist, movie) {
 
-    $scope.fetchLoggedUserWatchlist();
+        var watchlistToModify = $scope.userWatchlist.filter(function (watchlistInLocal) {
+            return watchlistInLocal.id == watchlist.id;
+        }).pop();
+        var movieToRemove = watchlistToModify.movies.filter(function (movieInLocalWatchlist) {
+            return movieInLocalWatchlist.trackId == movie.trackId;
+        }).pop();
+        movieToRemove.remove();
+
+        userWatchlistContainer.setUserWatchlist($scope.userWatchlist);
+    };
+
+    $scope.initLoggedUserWatchlist();
+
 });
