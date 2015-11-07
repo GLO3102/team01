4b0ca766fcd7dbdbbe291ee2b3e9a421e0ce70e6
Maxime Charron <charron.maxime97@gmail.com>
2015-11-06 19:09:52 -0500

    Fix centerMode for mobile

--- app/components/home/scripts/controller/home-controller.js
+++ app/components/home/scripts/controller/home-controller.js
@@ -47,10 +47,7 @@ homeApp.controller("home-controller", function ($scope, movieSelectionService, t
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    autoplay: true,
-                    autoplaySpeed: 5000,
+                    slidesToScroll: 1
                 }
             },
             {
@@ -58,8 +55,7 @@ homeApp.controller("home-controller", function ($scope, movieSelectionService, t
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 1,
-                    infinite: true,
-                    autoplay: true
+                    centerMode : false
                 }
             },
             {
@@ -67,8 +63,7 @@ homeApp.controller("home-controller", function ($scope, movieSelectionService, t
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
-                    infinite: true,
-                    autoplay: true
+                    centerMode: false
                 }
             }]
     };
@@ -97,8 +92,7 @@ homeApp.controller("home-controller", function ($scope, movieSelectionService, t
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 1,
-                    infinite: true,
-                    autoplay: true
+                    centerMode: false
                 }
             },
             {
@@ -106,8 +100,7 @@ homeApp.controller("home-controller", function ($scope, movieSelectionService, t
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
-                    infinite: true,
-                    autoplay: true
+                    centerMode: false
                 }
             }]
     }
