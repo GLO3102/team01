84886006d259e744bd7a05709917a18d776dd7b7
Pascal Dumoulin <dump02@gmail.com>
2015-12-11 14:50:32 -0500

    WIP -> Autocomplete

--- app/components/menu/scripts/controllers/menu-controller.js
+++ app/components/menu/scripts/controllers/menu-controller.js
@@ -3,7 +3,8 @@ menuApp.controller("menu-controller", function () {
 
     $(document).ready(function ($) {
 
-        $('.search-filters').hide();
+        $(".search-filters").hide();
+        $(".search-suggestions").hide();
 
         $('.search-input').on('click', function () {
             if ($('.search-filters').is(":hidden")) {
@@ -21,7 +22,41 @@ menuApp.controller("menu-controller", function () {
             var placeholder_text = $(this).closest('label').text();
             $('.search-input').attr('placeholder', 'search: ' + placeholder_text).focus();
         });
+
+        $("#search-bar").autocomplete({
+            minChars : 4,
+            lookup : function (query, done) {
+                $.ajax({
+                    type: "get",
+                    url: "https://itunes.apple.com/search",
+                    dataType: "jsonp",
+                    data:{
+                        //entity: "movie,tvSeason,tvEpisode,movieArtist",
+                        //attribute: "actorTerm,titleTerm",
+                        //media: "movie,tvShow",
+                        limit: "10",
+                        term: query
+                    },
+                    success : function (data){
+                        console.log(data);
+                        done(data);
+                    },
+                    transformResult: function(suggestion){
+                        var results ={
+                            suggestions: []
+                        };
+                        for (var i = 0; i < suggestion.resultCount; i++) {
+                            var name = suggestion.results[i].trackName || suggestion.results[i].collectionName || suggestion.results[i].artistName;
+                            results.suggestions.push({value:name, data: name});
+                        }
+                        return results
+                    }
+                    ,
+                    onSelect: function (suggestion) {
+                        $("#search-form").submit();
+                    }
+                });
+            }
+        });
     });
-
-
 });
